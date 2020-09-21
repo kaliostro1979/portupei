3572a13ca8348862c63882f5b09afded4ddaac9f
Misha <abachyanm@gmail.com>
2020-09-21 20:52:53 +0400

    In progress

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".section-2-slider").owlCarousel(
+        {
+            items: 7,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            loop:true,
+        }
+    );
+});
